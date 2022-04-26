98a6b067cb53914bbfebcf240ec9f20fbb018771
Jeremy Wilford <jeremydwilford@gmail.com>
2022-04-26 15:04:35 +0800

    wall of authors dialog

--- src/app/cafe/cafe-dialogs/wall-dialog/wall-dialog.component.ts
+++ src/app/cafe/cafe-dialogs/wall-dialog/wall-dialog.component.ts
@@ -6,6 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wall-dialog.component.scss']
 })
 export class WallDialogComponent implements OnInit {
+  authors = [
+    {
+      path: '../../../../assets/img/author3.png',
+      name: 'Leon “Smoky” Dilentanti',
+    },
+    {
+      path: '../../../../assets/img/author4.png',
+      name: 'Saffron Segovia',
+    },
+    {
+      path: '../../../../assets/img/author5.png',
+      name: 'Carl Winterhoven ',
+    },
+    {
+      path: '../../../../assets/img/author1.png',
+      name: 'Karl Muller',
+    },
+    {
+      path: '../../../../assets/img/author6.jpg',
+      name: 'Johannes Schlaf',
+    },
+    {
+      path: '../../../../assets/img/author7.png',
+      name: 'Gary Katsarida',
+    },
+    {
+      path: '../../../../assets/img/character5.png',
+      name: 'Maudy Gammitt',
+    },
+    {
+      path: '../../../../assets/img/author2.jpg',
+      name: 'Else Lasker-Schuler',
+    },
+    {
+      path: '../../../../assets/img/author8.png',
+      name: 'Josef Wozek',
+    },
+    {
+      path: '../../../../assets/img/character6.png',
+      name: 'Yitzhak Lowy',
+    }
+  ]
 
   constructor() { }
 
