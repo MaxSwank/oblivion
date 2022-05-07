79b48625e443366ce44831e5516a6c14823a035e
Jeremy Wilford <jeremydwilford@gmail.com>
2022-05-07 14:33:28 +0800

    landing page animations

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  cafeZoom: boolean = true;
+
+  constructor(private _router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  enterCafe() {
+    this.cafeZoom = !this.cafeZoom;
+    setTimeout(() => {
+      this._router.navigate(['/cafe']);
+    }, 1500);
   }
 
 }
