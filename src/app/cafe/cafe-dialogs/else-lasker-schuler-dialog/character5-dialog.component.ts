c1bd0f86cd8d627009c9ae0ad05a32cd6335629b
Jeremy Wilford <jeremydwilford@gmail.com>
2022-05-27 20:08:51 +0800

    mobile styling for character and typewriter dialogs. fixed carousel typescript, fixed cafe signs and mobile stying

--- src/app/cafe/cafe-dialogs/else-lasker-schuler-dialog/character5-dialog.component.ts
+++ src/app/cafe/cafe-dialogs/else-lasker-schuler-dialog/character5-dialog.component.ts
@@ -8,40 +8,52 @@ import { Component, OnInit } from '@angular/core';
 export class Character5DialogComponent implements OnInit {
   slides = [
     {
+      intro: '',
       quote: 'I perform ecstatic, mystic poems as ‘Yusof, Prince of Thebes.’ But at heart I’m really just a simple love poet.',
+      author: '',
+      title: ''
     },
     {
       intro: 'I overheard Junot telling someone that Oblivion is:',
       quote: 'A mesmerizing meditation on immortality, both the literary and fleshly kind and its ultimate unattainability … Like language, like summer, like love, Oblivion is irresistible.',
       author: 'Junot Díaz',
-      title: 'Winner of the Pulitzer Prize for Fiction',
+      title: 'Winner of the Pulitzer Prize for Fiction'
     },
     {
+      intro: '',
       quote: 'Józef is a good friend to me, but I suspect he is only interested in my work because Kafka came to see me perform a few times.',
+      author: '',
+      title: ''
     },
     {
       intro: 'Kim told me that Oblivion is:',
       quote: 'A delight to read … [E]very reader will appreciate the wonderful writing, the imaginative play between these worlds, and the serious exploration, just beneath the wildly entertaining surface, of the power of art to both shape us and sustain us.',
       author: 'Kim Edwards',
-      title: 'Author of The Memory Keeper’s Daughter and The Lake of Dreams',
+      title: 'Author of The Memory Keeper’s Daughter and The Lake of Dreams'
     },
     {
+      intro: '',
       quote: 'I understand Maudy has sold many books. Her work doesn’t speak to me personally, but I’ve been dead for many years.',
+      author: '',
+      title: ''
     },
     {
       intro: 'I overheard Sue saying that:',
       quote: 'Hemley’s masterful prose, and his willingness to completely inhabit this dream/world space, makes Oblivion a stunning and truly original book that not only succeeds on its own, but blazes a trail for other writers to follow.',
       author: 'Sue William Silverman',
-      title: 'Author of How to Survive Death and Other Inconveniences',
+      title: 'Author of How to Survive Death and Other Inconveniences'
     },
     {
+      intro: '',
       quote: 'New people are showing up all the time, so the café is always growing — but somehow we always seem to be short one chair.',
+      author: '',
+      title: ''
     },
     {
       intro: 'Xu Xi says that Oblivion is:',
       quote: 'Philosophical, engrossing and very, very funny, this book deserves its own shelf in libraries from here through eternity.  In particular for authors, minor or otherwise, this book is you.',
       author: 'Xu Xi',
-      title: 'Author of That Man in Our Lives and Habit of a Foreign Sky',
+      title: 'Author of That Man in Our Lives and Habit of a Foreign Sky'
     },
   ];
   slideConfig = { slidesToShow: 1, slidesToScroll: 1 };
--- src/app/cafe/cafe-dialogs/josef-wosek-dialog/character4-dialog.component.ts
+++ src/app/cafe/cafe-dialogs/josef-wosek-dialog/character4-dialog.component.ts
@@ -8,40 +8,52 @@ import { Component, OnInit } from '@angular/core';
 export class Character4DialogComponent implements OnInit {
   slides = [
     {
+      intro: '',
       quote: 'I’ve seen Else perform many times — she was an innovator, quite ahead of her time.',
+      author: '',
+      title: ''
     },
     {
       intro: 'Kirkus Reviews wrote that Oblivion is:',
       quote: 'The best kind of shaggy dog story, delightful in every particular.',
       author: 'Kirkus Reviews',
-      title: 'An American book review magazine',
+      title: 'An American book review magazine'
     },
     {
+      intro: '',
       quote: 'I’ve read more in the library of Oblivion than just about anyone here. I love the manuscripts intended to be books that never become books. I have some in there. As well as my published books, of course. But still, not many people read me.',
+      author: '',
+      title: ''
     },
     {
       intro: 'I overheard David calling Oblivion:',
       quote: '[S]urprisingly funny and always utterly, mortifyingly serious … Hemley’s best book — the book he was born to write and has been dying, as it were, to write his whole life. I truly admire and love the book.',
       author: 'David Shields',
-      title: 'Author of The Last Interview and Reality Hunger',
+      title: 'Author of The Last Interview and Reality Hunger'
     },
     {
+      intro: '',
       quote: 'Gary didn’t even land here right after death — he only got pulled up into the cafe when a lit professor at Northwestern made him mandatory reading.',
+      author: '',
+      title: ''
     },
     {
       intro: 'Mary says Oblivion is:',
       quote: 'A valiant, masterful act of imagination … In prose that is brisk, spare, and exceedingly funny, Robin Hemley takes us down corridors of literary oblivion, guided by the wry resolve of a narrator who finds his voice (and worth and audience) only after his own death.',
       author: 'Mary Cappello',
-      title: 'Author of Awkward: A Detour and Lecture',
+      title: 'Author of Awkward: A Detour and Lecture'
     },
     {
+      intro: '',
       quote: 'I offered to write Maudy’s biography because she’s nice and has such manners, but I rejected a story of hers at Hoot & Holler, so she might still be upset about that and say no. ',
+      author: '',
+      title: ''
     },
     {
       intro: 'Lee is telling everyone:',
       quote: '[It’s] the kind of book where you just don’t skip any lines, because surprises abound. Simply put, Oblivion is one of the best, most memorable books I’ve read in the last decade.',
       author: 'Lee Kofman',
-      title: 'Author of Imperfect and The Dangerous Bride',
+      title: 'Author of Imperfect and The Dangerous Bride'
     },
   ];
   slideConfig = { slidesToShow: 1, slidesToScroll: 1 };
