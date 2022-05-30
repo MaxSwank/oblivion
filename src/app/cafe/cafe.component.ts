7cc3b0cbd91ac0928d2c7130e55aa4271ba3af48
Troy Ross <troys.r.us@gmail.com>
2022-05-30 10:42:11 -0700

    Added scroll to right, cafe cleanup

--- src/app/cafe/cafe.component.ts
+++ src/app/cafe/cafe.component.ts
@@ -1,16 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { PreorderDialogComponent } from '../dialogs/preorder-dialog/preorder-dialog.component';
-import { ArbiterDialogComponent } from './cafe-dialogs/arbiter-dialog/arbiter-dialog.component';
-import { Character1DialogComponent } from './cafe-dialogs/bangs-dialog/character1-dialog.component';
-import { Character2DialogComponent } from './cafe-dialogs/maudy-gammitt-dialog/character2-dialog.component';
-import { Character3DialogComponent } from './cafe-dialogs/gary-katsarida-dialog/character3-dialog.component';
-import { Character4DialogComponent } from './cafe-dialogs/josef-wosek-dialog/character4-dialog.component';
-import { Character5DialogComponent } from './cafe-dialogs/else-lasker-schuler-dialog/character5-dialog.component';
-import { Character6DialogComponent } from './cafe-dialogs/character6-dialog/character6-dialog.component';
-import { TypewriterDialogComponent } from './cafe-dialogs/typewriter-dialog/typewriter-dialog.component';
-import { WallDialogComponent } from './cafe-dialogs/wall-dialog/wall-dialog.component';
-import { WindowDialogComponent } from './cafe-dialogs/window-dialog/window-dialog.component';
 
 
 @Component({
@@ -19,95 +8,11 @@ import { WindowDialogComponent } from './cafe-dialogs/window-dialog/window-dialo
   styleUrls: ['./cafe.component.scss'],
 })
 export class CafeComponent implements OnInit {
-  constructor(public dialog: MatDialog) {}
+  @ViewChild('cafecontainer') cafeContainer: ElementRef;
 
-  ngOnInit(): void {}
+  constructor(public dialog: MatDialog) { }
 
-  openTypewriterDialog() {
-    const dialogRef = this.dialog.open(TypewriterDialogComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openWindowDialog() {
-    const dialogRef = this.dialog.open(WindowDialogComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openWallDialog() {
-    const dialogRef = this.dialog.open(WallDialogComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openCharacter1Dialog() {
-    const dialogRef = this.dialog.open(Character1DialogComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openCharacter2Dialog() {
-    const dialogRef = this.dialog.open(Character2DialogComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openCharacter3Dialog() {
-    const dialogRef = this.dialog.open(Character3DialogComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openCharacter4Dialog() {
-    const dialogRef = this.dialog.open(Character4DialogComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openCharacter5Dialog() {
-    const dialogRef = this.dialog.open(Character5DialogComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openCharacter6Dialog() {
-    const dialogRef = this.dialog.open(Character6DialogComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openArbiterDialog() {
-    const dialogRef = this.dialog.open(ArbiterDialogComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  openPreorderDialog() {
-    const dialogRef = this.dialog.open(PreorderDialogComponent);
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-    });
+  ngOnInit() { 
+    setTimeout(() =>  this.cafeContainer.nativeElement.scrollLeft += 500, 400);
   }
 }
