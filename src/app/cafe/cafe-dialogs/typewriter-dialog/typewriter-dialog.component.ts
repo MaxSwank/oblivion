1acc6c4d3887fd3089c4d0389aa6742f2fd7e86f
Troy Ross <troys.r.us@gmail.com>
2022-05-30 20:11:59 -0700

    Formsubmit try #3

--- src/app/cafe/cafe-dialogs/typewriter-dialog/typewriter-dialog.component.ts
+++ src/app/cafe/cafe-dialogs/typewriter-dialog/typewriter-dialog.component.ts
@@ -37,7 +37,10 @@ export class TypewriterDialogComponent implements OnInit {
         'Content-Type': 'application/json'
       })
     };
-    this.httpClient.post('https://formsubmit.co/ajax/457bcf42af5ec0b2de887fc89f30b371', formData, httpOptions)
-      .subscribe((response) => this.location.back(), (error) => console.log("Error:", error));
+    this.httpClient.post('https://formsubmit.co/ajax/oblivion.by.robin.hemley@gmail.com', formData, httpOptions)
+      .subscribe((response) => { 
+        console.log("Response:", response); 
+        this.location.back();
+      }, (error) => console.log("Error:", error));
   }
 }
